570f7ad511bb8e07568ccbb275437b5da17a274c
pisit.namsiri@gmail.com <pisit.namsiri@gmail.com>
2023-08-20 19:07:09 +0700

    no message

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,10 @@
-const ims = require('./monitorModel');
-const ihs = require('./historyModel');
-const its = require('./transactionModel');
+// const ic_histories = require('./ic_histories');
+// const ic_transactions = require('./ic_transactions');
+// const ic_monitors = require('./ic_monitors');
 
-module.exports = { ims, ihs, its };
+// ic_monitors.hasMany(ic_histories);
+// ic_monitors.hasMany(ic_transactions);
+// ic_histories.belongsTo(ic_monitors);
+// ic_transactions.belongsTo(ic_monitors);
+
+// module.exports = { ic_monitors, ic_histories, ic_transactions };
